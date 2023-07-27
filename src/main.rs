d5f0c2b3fe88d7b8c9b3b2c1e24136611ba490fe
romnnn <roman.dahm@gmx.de>
2023-07-27 21:12:56 +0200

    initialize logger lazily

--- src/main.rs
+++ src/main.rs
@@ -44,7 +44,45 @@ fn main() -> eyre::Result<()> {
     let options = Options::parse();
     #[cfg(debug_assertions)]
     std::env::set_var("RUST_BACKTRACE", "full");
-    let _stats = casimu::ported::accelmain(&options.trace_dir, options.stats_out_file.as_ref())?;
+
+    let mut log_builder = env_logger::Builder::new();
+    log_builder.format(|buf, record| {
+        writeln!(
+            buf,
+            // "{} [{}] - {}",
+            "{}",
+            // Local::now().format("%Y-%m-%dT%H:%M:%S"),
+            // record.level(),
+            record.args()
+        )
+    });
+
+    let log_after_cycle = std::env::var("LOG_AFTER")
+        .unwrap_or_default()
+        .parse::<u64>()
+        .ok();
+
+    if log_after_cycle.is_none() {
+        log_builder.parse_default_env();
+        log_builder.init();
+    }
+
+    let stats = casimu::ported::accelmain(&options.trace_dir, log_after_cycle)?;
+
+    // save stats to file
+    if let Some(stats_out_file) = options.stats_out_file.as_ref() {
+        casimu::ported::save_stats_to_file(&stats, &stats_out_file)?;
+    }
+
+    eprintln!("STATS:\n");
+    eprintln!("DRAM: total reads: {}", &stats.dram.total_reads());
+    eprintln!("DRAM: total writes: {}", &stats.dram.total_writes());
+    eprintln!("SIM: {:#?}", &stats.sim);
+    eprintln!("INSTRUCTIONS: {:#?}", &stats.instructions);
+    eprintln!("ACCESSES: {:#?}", &stats.accesses);
+    eprintln!("L1I: {:#?}", &stats.l1i_stats.reduce());
+    eprintln!("L1D: {:#?}", &stats.l1d_stats.reduce());
+    eprintln!("L2D: {:#?}", &stats.l2d_stats.reduce());
     eprintln!("completed in {:?}", start.elapsed());
     Ok(())
 }
