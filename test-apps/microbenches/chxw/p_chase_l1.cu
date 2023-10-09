2fcd658c84591e15f95e5e8b56c689a567b44eb3
romnnn <roman.dahm@gmx.de>
2023-10-09 07:47:50 +0200

    gpucachesim

--- test-apps/microbenches/chxw/p_chase_l1.cu
+++ test-apps/microbenches/chxw/p_chase_l1.cu
@@ -1,5 +1,6 @@
 #include <assert.h>
 #include <cstdlib>
+#include <ctype.h>
 #include <stdint.h>
 #include <stdio.h>
 
@@ -22,18 +23,6 @@ const bool USE_COMPRESSION = false;
 
 const int KB = 1024;
 
-// have 96 KB shared memory, sizeof(unsigned)
-// const static size_t SHMEM_SIZE_BYTES = 0xC000;
-
-// have only 48KB shared mem because we test the 16KB L1?
-// const size_t SHMEM_SIZE_BYTES = 16 * (1 << 10);
-// const int ITER_SIZE = 512;
-// const int ITER_SIZE = 4 * 1024;
-// const int ITER_SIZE = 8 * 1024;
-// const int ITER_SIZE = 6 * 1024;
-// const int ITER_SIZE = (SHMEM_SIZE_BYTES / 2) / sizeof(unsigned int);
-
-// const int ITER_SIZE = 6 * 1024;
 const int ITER_SIZE = ((48 * KB) / 2) / sizeof(uint32_t);
 
 __global__ void global_latency_l1_data(unsigned int *array, int array_length,
@@ -41,7 +30,6 @@ __global__ void global_latency_l1_data(unsigned int *array, int array_length,
                                        unsigned int *index,
                                        size_t warmup_iterations) {
   unsigned int start_time, end_time;
-  uint32_t warmup_j = 0;
   uint32_t j = 0;
 
   __shared__ uint32_t s_tvalue[ITER_SIZE];
@@ -62,10 +50,10 @@ __global__ void global_latency_l1_data(unsigned int *array, int array_length,
       s_tvalue[k] = end_time - start_time;
     } else {
       j = array[j];
-      // warmup_j = array[warmup_j];
     }
   }
 
+  // store to avoid caching in readonly?
   array[array_length] = j;
   array[array_length + 1] = array[j];
 
@@ -163,12 +151,65 @@ __global__ void global_latency_compressed(unsigned int *array, int array_length,
   }
 }
 
+const int READONLY_ITER_SIZE = (48 * KB) / sizeof(uint32_t);
+
 __global__ void
 global_latency_l1_readonly(const unsigned int *__restrict__ array,
                            int array_length, unsigned int *duration,
                            unsigned int *index, size_t warmup_iterations) {
+  const int iter_size = READONLY_ITER_SIZE;
   unsigned int start_time, end_time;
-  uint32_t j = threadIdx.x;
+  size_t it;
+  // uint32_t j = threadIdx.x;
+  uint32_t j = 0;
+
+  __shared__ uint32_t s_tvalue[iter_size];
+  // __shared__ uint32_t s_index[ITER_SIZE];
+
+  for (size_t k = 0; k < iter_size; k++) {
+    // s_index[k] = 0;
+    s_tvalue[k] = 0;
+  }
+
+  // no-timing iterations, for large arrays
+  for (it = 0; it < warmup_iterations * iter_size; it++) {
+    j = __ldg(&array[j]);
+  }
+
+  // for (int it = (int)warmup_iterations * -ITER_SIZE; it < ITER_SIZE; it++) {
+  for (it = 0; it < iter_size / 32; it++) {
+    int k = it * blockDim.x + threadIdx.x;
+    start_time = clock();
+    j = __ldg(&array[j]);
+    // s_index[k] = j;
+    // s_tvalue[k] = j;
+    s_tvalue[iter_size - 1] = j;
+    end_time = clock();
+
+    s_tvalue[k] = end_time - start_time;
+  }
+
+  // cannot no longer write to it!
+  // array[array_length] = j;
+  // array[array_length + 1] = array[j];
+
+  for (it = 0; it < iter_size / 32; it++) {
+    int k = it * blockDim.x + threadIdx.x;
+    // index[k] = s_index[k];
+    duration[k] = s_tvalue[k];
+  }
+}
+
+// declare the texture
+// texture<int, 1, cudaReadModeElementType> tex_ref;
+
+__global__ void
+global_latency_l1_texture(unsigned int *array, cudaTextureObject_t tex,
+                          int array_length, unsigned int *duration,
+                          unsigned int *index, size_t warmup_iterations) {
+  unsigned int start_time, end_time;
+  uint32_t j = 0;
+  // uint32_t j = threadIdx.x;
 
   __shared__ uint32_t s_tvalue[ITER_SIZE];
   __shared__ uint32_t s_index[ITER_SIZE];
@@ -179,31 +220,50 @@ global_latency_l1_readonly(const unsigned int *__restrict__ array,
   }
 
   for (int it = (int)warmup_iterations * -ITER_SIZE; it < ITER_SIZE; it++) {
+    // int k = it * blockDim.x + threadIdx.x;
+    int k = it;
     if (it >= 0) {
-      int k = it * blockDim.x + threadIdx.x;
       start_time = clock();
-      j = __ldg(&array[j]);
+      // j = tex1Dfetch(tex_ref, j);
+      j = tex1Dfetch<unsigned int>(tex, j);
+      // j = __ldg(&array[j]);
       s_index[k] = j;
       end_time = clock();
 
       s_tvalue[k] = end_time - start_time;
     } else {
-      j = __ldg(&array[j]);
+      j = tex1Dfetch<unsigned int>(tex, j);
+      // j = tex1Dfetch(tex_ref, j);
+      // j = __ldg(&array[j]);
     }
   }
 
   // array[array_length] = j;
   // array[array_length + 1] = array[j];
 
-  for (size_t it = 0; it < ITER_SIZE; it++) {
-    int k = it * blockDim.x + threadIdx.x;
-    index[k] = s_index[k];
-    duration[k] = s_tvalue[k];
+  size_t it = 0;
+  for (; it < ITER_SIZE; it++) {
+    // int k = it * blockDim.x + threadIdx.x;
+    // int k = it;
+    index[it] = s_index[it];
+    duration[it] = s_tvalue[it];
   }
+
+  // why is this so different?
+  array[array_length] = it;
+  array[array_length + 1] = s_tvalue[it - 1];
 }
 
-void parametric_measure_global(size_t N, size_t stride,
-                               size_t warmup_iterations) {
+enum memory { L1Data, L1ReadOnly, L1Texture, L2, NUM_MEMORIES };
+const char *memory_str[NUM_MEMORIES] = {
+    "l1data",
+    "l1readonly",
+    "l1texture",
+    "l2",
+};
+
+int parametric_measure_global(memory mem, size_t N, size_t stride,
+                              size_t warmup_iterations) {
   cudaDeviceReset();
 
   // print CSV header
@@ -228,36 +288,95 @@ void parametric_measure_global(size_t N, size_t stride,
 
   // copy array elements from CPU to GPU
   CUDA_SAFECALL(
-      cudaMemcpy(d_a, h_a, sizeof(unsigned int) * N, cudaMemcpyHostToDevice));
+      cudaMemcpy(d_a, h_a, N * sizeof(unsigned int), cudaMemcpyHostToDevice));
 
-  size_t iter_size = USE_COMPRESSION ? ITER_SIZE_COMPRESSED : ITER_SIZE;
+  size_t iter_size;
+  switch (mem) {
+  case L1ReadOnly:
+    iter_size = READONLY_ITER_SIZE;
+    break;
+  case L1Data:
+  case L1Texture:
+  case L2:
+    iter_size = USE_COMPRESSION ? ITER_SIZE_COMPRESSED : ITER_SIZE;
+    break;
+  case NUM_MEMORIES:
+    assert(false && "panic dispatching to memory");
+  };
 
   unsigned int *h_index =
-      (unsigned int *)malloc(sizeof(unsigned int) * iter_size);
+      (unsigned int *)malloc(iter_size * sizeof(unsigned int));
   unsigned int *h_timeinfo =
-      (unsigned int *)malloc(sizeof(unsigned int) * iter_size);
+      (unsigned int *)malloc(iter_size * sizeof(unsigned int));
 
   unsigned int *duration;
   CUDA_SAFECALL(
-      cudaMalloc((void **)&duration, sizeof(unsigned int) * iter_size));
+      cudaMalloc((void **)&duration, iter_size * sizeof(unsigned int)));
 
   unsigned int *d_index;
   CUDA_SAFECALL(
-      cudaMalloc((void **)&d_index, sizeof(unsigned int) * iter_size));
+      cudaMalloc((void **)&d_index, iter_size * sizeof(unsigned int)));
+
+  cudaTextureObject_t texObj = 0;
 
   cudaDeviceSynchronize();
   // launch kernel
   dim3 block_dim = dim3(1);
   dim3 grid_dim = dim3(1, 1, 1);
 
-  if (USE_COMPRESSION) {
-    CUDA_SAFECALL((global_latency_compressed<<<grid_dim, block_dim>>>(
-        d_a, N, duration, d_index, warmup_iterations)));
-  } else {
-    CUDA_SAFECALL((global_latency_l1_data<<<grid_dim, block_dim>>>(
-        d_a, N, duration, d_index, warmup_iterations)));
-  }
+  switch (mem) {
+  case L1Texture:
+    // bind texture
+    // cudaBindTexture(0, tex_ref, d_a, N * sizeof(int));
 
+    // Specify texture
+    struct cudaResourceDesc resDesc;
+    memset(&resDesc, 0, sizeof(resDesc));
+    // resDesc.resType = cudaResourceTypeArray;
+    // resDesc.res.array.array = d_a;
+
+    resDesc.resType = cudaResourceTypeLinear;
+    resDesc.res.linear.devPtr = d_a;
+    resDesc.res.linear.desc.f = cudaChannelFormatKindUnsigned;
+    resDesc.res.linear.desc.x = 32; // bits per channel
+    resDesc.res.linear.sizeInBytes = N * sizeof(unsigned int);
+
+    // Specify texture object parameters
+    struct cudaTextureDesc texDesc;
+    memset(&texDesc, 0, sizeof(texDesc));
+    // texDesc.addressMode[0] = cudaAddressModeWrap;
+    // texDesc.addressMode[1] = cudaAddressModeWrap;
+    // texDesc.filterMode = cudaFilterModeLinear;
+    // texDesc.readMode = cudaReadModeElementType;
+    texDesc.normalizedCoords = 1;
+
+    // Create texture object
+    cudaCreateTextureObject(&texObj, &resDesc, &texDesc, NULL);
+
+    cudaDeviceSynchronize();
+
+    CUDA_SAFECALL((global_latency_l1_texture<<<grid_dim, block_dim>>>(
+        d_a, texObj, N, duration, d_index, warmup_iterations)));
+    break;
+  case L1ReadOnly:
+    block_dim = dim3(32, 1, 1);
+    CUDA_SAFECALL((global_latency_l1_readonly<<<grid_dim, block_dim>>>(
+        d_a, N, duration, d_index, warmup_iterations)));
+    break;
+  case L2:
+  case L1Data:
+    if (USE_COMPRESSION) {
+      CUDA_SAFECALL((global_latency_compressed<<<grid_dim, block_dim>>>(
+          d_a, N, duration, d_index, warmup_iterations)));
+    } else {
+      CUDA_SAFECALL((global_latency_l1_data<<<grid_dim, block_dim>>>(
+          d_a, N, duration, d_index, warmup_iterations)));
+    }
+    break;
+  default:
+    assert(false && "panic dispatching to memory");
+    break;
+  };
   cudaDeviceSynchronize();
 
   CUDA_SAFECALL(cudaGetLastError());
@@ -274,28 +393,64 @@ void parametric_measure_global(size_t N, size_t stride,
 
   cudaDeviceSynchronize();
 
-  if (USE_COMPRESSION) {
-    unsigned int j = 0;
-    // fprintf(stdout, "%4d to %4d\n", (int)warmup_iterations * -(int)iter_size,
-    //         (int)iter_size);
-    for (int k = (int)warmup_iterations * -(int)iter_size; k < (int)iter_size;
-         k++) {
-      if (k >= 0) {
-        j = h_a[j];
-        unsigned int index = j;
-        unsigned int binned_latency = h_timeinfo[k] * LATENCY_BIN_SIZE;
-        fprintf(stdout, "%4d,%4d\n", index, binned_latency);
-      } else {
-        j = h_a[j];
+  unsigned int j = 0;
+
+  switch (mem) {
+  case L1ReadOnly:
+    for (size_t it = 0; it < warmup_iterations * iter_size; it++) {
+      j = h_a[j];
+    }
+
+    // for (it = 0; it < ITER_SIZE / 32; it++) {
+    //   int k = it * blockDim.x + threadIdx.x;
+    //   // index[k] = s_index[k];
+    //   duration[k] = s_tvalue[k];
+    // }
+
+    for (size_t k = 0; k < iter_size / 32; k++) {
+      j = h_a[j];
+      unsigned int index = j;
+      // unsigned int index = h_index[k * 32];
+      float mean_latency = 0;
+      for (size_t t = 0; t < 32; t++) {
+        // if (h_index[k * 32 + t] != index) {
+        //   fprintf(
+        //       stderr,
+        //       "threads %lu and %lu accessed different indices (%4d !=
+        //       %4d)\n", k * 32, k * 32 + t, index, h_index[k * 32 + t]);
+        //   return EXIT_FAILURE;
+        // }
+
+        mean_latency += (float)h_timeinfo[k * 32];
+      }
+      mean_latency /= 32.0;
+      fprintf(stdout, "%4d,%4.10f\n", index, mean_latency);
+    }
+    break;
+  default:
+    if (USE_COMPRESSION) {
+      // unsigned int j = 0;
+      for (int k = (int)warmup_iterations * -(int)iter_size; k < (int)iter_size;
+           k++) {
+        if (k >= 0) {
+          j = h_a[j];
+          unsigned int index = j;
+          unsigned int binned_latency = h_timeinfo[k] * LATENCY_BIN_SIZE;
+          fprintf(stdout, "%4d,%4d\n", index, binned_latency);
+        } else {
+          j = h_a[j];
+        }
+      }
+    } else {
+      for (size_t k = 0; k < iter_size; k++) {
+        unsigned int index = h_index[k];
+        unsigned int latency = h_timeinfo[k];
+        fprintf(stdout, "%4d,%4d\n", index, latency);
       }
     }
-  } else {
-    for (size_t k = 0; k < iter_size; k++) {
-      unsigned int index = h_index[k];
-      unsigned int latency = h_timeinfo[k];
-      fprintf(stdout, "%4d,%4d\n", index, latency);
-    }
+    break;
   }
+
   // unsigned int j = 0;
   // for (size_t k = 0; k < iter_size ; k++) {
   //   // print as CSV to stdout
@@ -320,6 +475,11 @@ void parametric_measure_global(size_t N, size_t stride,
   //   fprintf(stdout, "%4d,%4d\n", index, latency);
   // }
 
+  // destroy texture object
+  if (texObj != 0) {
+    cudaDestroyTextureObject(texObj);
+  }
+
   // free memory on GPU
   cudaFree(d_a);
   cudaFree(d_index);
@@ -331,20 +491,52 @@ void parametric_measure_global(size_t N, size_t stride,
   free(h_timeinfo);
 
   cudaDeviceReset();
+
+  return EXIT_SUCCESS;
 }
 
 int main(int argc, char *argv[]) {
   cudaSetDevice(0);
+  memory mem;
   size_t size_bytes, stride_bytes, warmup_iterations;
 
   // parse arguments
-  if (argc > 2) {
-    size_bytes = atoi(argv[1]);
-    stride_bytes = atoi(argv[2]);
-    warmup_iterations = atoi(argv[3]);
+  if (argc >= 5) {
+    char *mem_name = argv[1];
+
+    // make mem name lowercase
+    for (int i = 0; mem_name[i]; i++) {
+      mem_name[i] = tolower(mem_name[i]);
+    }
+    int k = 0;
+    int *mem_found = NULL;
+    for (; k < NUM_MEMORIES; k++) {
+      if (!strcmp(mem_name, memory_str[k])) {
+        mem_found = &k;
+        break;
+      }
+    }
+
+    if (mem_found == NULL) {
+      fprintf(stderr, "unknown memory name %s\n", mem_name);
+      return EXIT_FAILURE;
+    }
+
+    mem = (memory)(*mem_found);
+    size_bytes = atoi(argv[2]);
+    stride_bytes = atoi(argv[3]);
+    warmup_iterations = atoi(argv[4]);
   } else {
     fprintf(stderr,
-            "usage: p_chase_l1 <SIZE_BYTES> <STRIDE_BYTES> <WARMUP> \n");
+            "usage: p_chase_l1 <MEM> <SIZE_BYTES> <STRIDE_BYTES> <WARMUP>\n\n");
+    fprintf(stderr, "    <MEM>:\t");
+    for (int k = 0; k < NUM_MEMORIES; k++) {
+      fprintf(stderr, "%s", memory_str[k]);
+      if (k + 1 < NUM_MEMORIES) {
+        fprintf(stderr, " | ");
+      }
+    }
+    fprintf(stderr, "\n\n\n");
     return EXIT_FAILURE;
   }
 
@@ -358,10 +550,11 @@ int main(int argc, char *argv[]) {
   size_t size = size_bytes / sizeof(uint32_t);
   size_t stride = stride_bytes / sizeof(uint32_t);
 
+  fprintf(stderr, "\tMEMORY             = %s\n", memory_str[mem]);
   fprintf(stderr,
           "\tSIZE               = %10lu bytes (%10lu uint32, %10.4f KB)\n",
           size_bytes, size, (float)size_bytes / 1024.0);
-  fprintf(stderr, "\tSTRIDE             = %10lu bytes (%10lu uint32)\n ",
+  fprintf(stderr, "\tSTRIDE             = %10lu bytes (%10lu uint32)\n",
           stride_bytes, stride);
   fprintf(stderr, "\tROUNDS             = %3.3f\n", num_rounds);
   fprintf(stderr, "\tONE ROUND          = %3.3f (have %5lu)\n", one_round,
@@ -398,11 +591,6 @@ int main(int argc, char *argv[]) {
     return EXIT_FAILURE;
   }
 
-  // printf("\n=====%10.4f KB array, warm TLB, read iter_size element====\n",
-  //          sizeof(unsigned int) * (float)N / 1024);
-  //   printf("Stride = %d element, %d byte\n", stride,
-  //          stride * sizeof(unsigned int));
-
   // The `cudaDeviceSetCacheConfig` function can be used to set preference for
   // shared memory or L1 cache globally for all CUDA kernels in your code and
   // even those used by Thrust.
@@ -435,10 +623,11 @@ int main(int argc, char *argv[]) {
           prop.sharedMemPerMultiprocessor);
   fprintf(stderr, "\tL2 size           = %u\n", prop.l2CacheSize);
 
-  parametric_measure_global(size, stride, warmup_iterations);
+  int exit_code =
+      parametric_measure_global(mem, size, stride, warmup_iterations);
 
   cudaDeviceReset();
   fflush(stdout);
   fflush(stderr);
-  return EXIT_SUCCESS;
+  return exit_code;
 }
