334cf94047211a00abcd98d148d27a0cb1a13354
romnnn <roman.dahm@gmx.de>
2023-11-11 20:04:17 +0100

    fix cuda module on das5 cluster

--- test-apps/build_pchase.sh
+++ test-apps/build_pchase.sh
@@ -1,6 +1,25 @@
 #!/bin/sh
 
-module load cuda11.7/toolkit
+set -e
+
+# try das6 first, then das 5
+module load cuda11.7/toolkit || module load cuda11.1/toolkit/11.1.1
+
+# DAS 5
+# -C GTX980
+# nodes with an Nvidia GTX980, Maxwell 48 KB L1, 2 MB L2
+# -C Titan
+# nodes with an Nvidia GTX Titan, Kepler 16 KB L1, 1536 KB L2
+# -C K20
+# nodes with an Nvidia Tesla K20, Kepler 16 KB L1, 1280 KB L2
+# -C RTX2080Ti
+# nodes with an Nvidia RTX 2080 Ti, Turing, 64 KB L1, 5.5 MB L2
+# -C TitanX
+# nodes with an Nvidia GTX TitanX, Maxwell, 48 KB L1, 3 MB L2
+# -C TitanX-Pascal
+# nodes with an Nvidia GTX TitanX, Pascal, 48 KB L1, 3 MB L2
+
+# we could test TitanX-Pascal, TitanX, GTX980, Titan
 
 SCRATCH="/var/scratch/rdm420/"
 REPO="$SCRATCH/gpucachesim/"
